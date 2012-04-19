6179e16f7f0e29a67e4dcd94ce5fe744ba60b585
blanquett <blanquett@me.com>
2012-04-19 19:41:16 +0000

    Plain Project

---
+++ OpenLaszlo2HTML5/OpenLaszlo2HTML5/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpenLaszlo2HTML5
+//
+//  Created by Matthias Blanquett on 19.04.12.
+//  Copyright (c) 2012 Buhl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}

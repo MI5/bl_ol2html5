d15d4f805015496d41847a30479a4826cb983ffb
blanquett <blanquett@8a5fa128-bb7a-43e4-82e0-066bdbbf7eae>
2012-04-19 19:41:16 +0000

    Plain Project

---
+++ trunk/OpenLaszlo2HTML5/OpenLaszlo2HTML5/ol2hAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ol2hAppDelegate.h
+//  OpenLaszlo2HTML5
+//
+//  Created by Matthias Blanquett on 19.04.12.
+//  Copyright (c) 2012 Buhl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ol2hAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ trunk/OpenLaszlo2HTML5/OpenLaszlo2HTML5/ol2hAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ol2hAppDelegate.m
+//  OpenLaszlo2HTML5
+//
+//  Created by Matthias Blanquett on 19.04.12.
+//  Copyright (c) 2012 Buhl. All rights reserved.
+//
+
+#import "ol2hAppDelegate.h"
+
+@implementation ol2hAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end

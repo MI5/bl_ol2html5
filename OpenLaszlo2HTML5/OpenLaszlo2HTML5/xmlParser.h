2c4b22ecdf13388b667aee795d421da54b2bd6ae
blanquett <blanquett@me.com>
2012-11-27 12:04:51 +0000

    Vor entfernen der Methode 'legeDatasetAnUndInitMitOeffnendemTag' und zusammenfassen aller dataset-Instanzierungen

--- OpenLaszlo2HTML5/OpenLaszlo2HTML5/xmlParser.h
+++ OpenLaszlo2HTML5/OpenLaszlo2HTML5/xmlParser.h
@@ -17,7 +17,6 @@
 -(id)initWith:(NSURL*) pathToFile;
 
 // Weil wir uns auch rekursiv aufrufen, muss ich zwischendruch ein Array mit den Zwischenergebnissen zurückgeben
-// Array enthält derzeit 6 Objekte (5 Strings und unsere intern gesammelten JS-Variablen
 -(NSArray*) start;
 
 
